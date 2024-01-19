ffe4ff16c52facf3dbf810fd84c9c6477aa0076f
rashadrabby <rashadulislamrabby@gmail.com>
2024-01-19 23:26:18 +0600

    'new_project'

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/Component/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar/>
+      <h1 className="text-5xl text-center mt-6 text-red-500">Welcome to SoftyCraft</h1>
+    </main>
+  )
+}
