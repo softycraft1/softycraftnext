d576de1a62858949527f20dd740b1cfc3be702f1
AbuKhairAnis <abukhairanis123@gmail.com>
2024-01-28 00:58:47 +0600

    Project page and components

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,45 +1,46 @@
-"use client"
-
 import { getProjectData } from "@/lib/projectdata/projectdata";
 import Image from "next/image";
-import { Suspense, useState } from "react";
+import Link from "next/link";
+import { Suspense } from "react";
 
+const Projects = async () => {
+  const datas = (await getProjectData()).slice(1, 6);
 
-const Projects = () => {
-    const [data, setData] = useState(first_data);
-
-    async function getData() {
-      setData(await getProjectData(type));
-    }
-  
-    console.log(getProjectData());
-    return (
-        <div>
-            <div>
-
-            </div>
-            <div className="">
-          <Suspense fallback={<h1>Loading..........</h1>}>
-            {data.map((d, i) => (
-              <div
-                key={i}
-                className="h-40 bg-[#ececec9d] odd:bg-[#4bb4774d] hover:bg-[#85d46d9c] p-8 m-2 "
-              >
-                <div className="flex flex-col justify-left items-start gap-1">
-                  <div className="flex justify-center items-center gap-3">
-                    <Image src={d.picture} alt="icon" width={350} height={350} />
-                    <h2 className="font-bold">{d.title}</h2>
-                  </div>
-                  <div>
-                    <p className="font-normal">{d.desc}</p>
-                  </div>
+  return (
+    <div>
+      <div className="">
+        <Suspense fallback={<h1>Loading..........</h1>}>
+          {datas.map((data) => (
+            <div
+              key={data.id}
+              className="h-40 bg-[#ececec9d] odd:bg-[#4bb4774d] hover:bg-[#85d46d9c] p-8 m-2 "
+            >
+              <div className="flex flex-col justify-left items-start gap-1">
+                <div className="flex justify-center items-center gap-3">
+                  <Image
+                    src={data.picture}
+                    alt="icon"
+                    width={350}
+                    height={350}
+                  />
+                  <h2 className="font-bold">{data.title}</h2>
+                </div>
+                <div>
+                  <p className="font-normal">{data.desc}</p>
                 </div>
               </div>
-            ))}
-          </Suspense>
-        </div>
-        </div>
-    );
+            </div>
+          ))}
+        </Suspense>
+      </div>
+      <div>
+        <Link className="btn2" href="/projects">
+          {" "}
+          Show More.........
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
