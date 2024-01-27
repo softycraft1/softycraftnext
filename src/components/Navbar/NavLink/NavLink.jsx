06bf2feec628dddb75f1dfcf53e564bb235ac9a2
rocktohq <itzmonir@gmail.com>
2024-01-27 22:51:28 +0600

    Navbar Updated

--- src/components/Navbar/NavLink/NavLink.jsx
+++ src/components/Navbar/NavLink/NavLink.jsx
@@ -6,7 +6,6 @@ import style from "./NavLink.module.css";
 import Image from "next/image";
 import { useState } from "react";
 
-
 function NavLink() {
   const pathname = usePathname();
 
@@ -52,7 +51,7 @@ function NavLink() {
           href={link.url}
           className={`${
             pathname === link.url && style.active
-          } w-8 h-8 p-2 px-4 mx-1 hover:border-2 hover:border-none hover:rounded-[10px] hover:bg-[#f8b90c7a] hover:drop-shadow-md`}
+          } w-8 h-8 p-2 px-4 mx-1 hover:bg-[#1a1a1a] hover:text-white duration-200 hover:drop-shadow-md`}
         >
           {link.title}
         </Link>
